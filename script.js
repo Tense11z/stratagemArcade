f359b0da251be08664e129f39ca2972454685e5b
Tense11z <tensei.60@gmail.com>
2024-05-18 01:46:50 +0300

    fixed node issue and timer

--- script.js
+++ script.js
@@ -47,6 +47,8 @@ let currentScore = 0;
 let stratagemPerRoundAmount = 2;
 let stratagemID = 0;
 let secondsLeft = 5;
+// Add a variable to store the interval ID
+let timerInterval;
 
 
 // function switchToGameEventListener() {
@@ -59,59 +61,13 @@ let secondsLeft = 5;
 //     document.addEventListener('keydown', handleKeyDownForMenu);
 // }
 
-function initMenu() {
-    gameOverLeaderboard.style.display = 'none';
-    document.addEventListener('keydown', handleKeyDownForMenu);
-    startScreen.style.display = 'block';
-    document.removeEventListener('keydown', handleKeyDownForGame);
-}
-
-
-// function to initialize the game
-function initGame() {
-    document.addEventListener('keydown', handleKeyDownForGame);
-    startRound();
-    const container = createContainer();
-    document.body.appendChild(container);
-    stratagemNameDisplay.textContent = Object.keys(stratagems).find(key => stratagems[key] === roundStratagemList[stratagemID]); // doesnt work
-    timeDisplay.textContent = secondsLeft;
-    playerScore.textContent = currentScore;
-    // gameScreen = document.querySelector('.gameScreen');
-    // gameScreen.style.display = 'block';
-}
-
-// function to pull 6 stratagems in a round
-function startRound() {
-    roundStratagemList = [];
-    const stratagemNames = Object.keys(stratagems);
-    for (let i = 0; i < stratagemPerRoundAmount; i += 1) {
-        roundStratagemList.push(stratagems[stratagemNames[Math.floor(Math.random() * stratagemNames.length)]]);
-    }
-    console.log(stratagemPerRoundAmount);
-}
-
-// function to create a new container or recreate existing one
-function createContainer() {
-    let containerToRemove = document.querySelector('.gameScreen');
-    if (containerToRemove != null) {
-        document.body.removeChild(containerToRemove);
-        let container = createContainer();
-        document.body.appendChild(container);
-        stratagemNameDisplay.textContent = Object.keys(stratagems).find(key => stratagems[key] === roundStratagemList[stratagemID]);
-    } else {
-        const container = createArrowDivs();
-        container.classList.add('gameScreen');
-        return container;
-    }
-}
-
-//functions that creates timer for a round
 function timer() {
-    let timer = setInterval(function () {
-        timeDisplay.innerHTML = '00:' + secondsLeft;
+    secondsLeft = 5;
+    timerInterval = setInterval(function () {
+        timeDisplay.innerHTML = '00:' + (secondsLeft < 10 ? '0' : '') + secondsLeft;
         secondsLeft--;
         if (secondsLeft < 0) {
-            clearInterval(timer);
+            clearInterval(timerInterval);
             console.log('Time is out');
             gameOverLeaderboard.style.display = 'block';
             document.removeEventListener('keydown', handleKeyDownForGame);
@@ -120,16 +76,56 @@ function timer() {
             setTimeout(function () {
                 initMenu();
             }, 5000);
-
         }
-    }, 1000)
+    }, 1000);
+}
 
+function createContainer() {
+    let container = document.querySelector('.gameScreen');
+    if (container != null) {
+        document.body.removeChild(container);
+    }
+    container = createArrowDivs();
+    container.classList.add('gameScreen');
+    return container;
+}
+
+// function to initialize the game
+function initGame() {
+    document.addEventListener('keydown', handleKeyDownForGame);
+    startRound();
+    updateStratagemDisplay();
+    const container = createContainer();
+    document.body.appendChild(container);
+    timeDisplay.textContent = secondsLeft;
+    playerScore.textContent = currentScore;
+}
+
+// function to pull stratagems for a round
+function startRound() {
+    if (!timeDisplay.classList.contains('startTimer')) {
+        timeDisplay.classList.add('startTimer');
+        timer();
+    } else {
+        timer();
+    }
+    roundStratagemList = [];
+    const stratagemNames = Object.keys(stratagems);
+    for (let i = 0; i < stratagemPerRoundAmount; i += 1) {
+        roundStratagemList.push(stratagems[stratagemNames[Math.floor(Math.random() * stratagemNames.length)]]);
+    }
+    console.log(roundStratagemList); // for debugging
+}
+
+// Function to update the displayed stratagem name
+function updateStratagemDisplay() {
+    const stratagemName = Object.keys(stratagems).find(key => stratagems[key] === roundStratagemList[stratagemID]);
+    stratagemNameDisplay.textContent = stratagemName;
 }
 
 // function that creates divs for each arrow in a stratagem sequence
 function createArrowDivs() {
     let randomStratagemArrows = roundStratagemList[stratagemID];
-
     const container = document.createElement('div');
     randomStratagemArrows.forEach((arrow, index) => {
         const arrowDiv = document.createElement('div');
@@ -150,11 +146,6 @@ function createArrowDivs() {
 // Function to handle keydown events
 function handleKeyDownForGame(e) {
     if (secondsLeft >= 0) {
-        if (!timeDisplay.classList.contains('startTimer')) {
-            timeDisplay.classList.add('startTimer');
-            timer();
-        }
-
         // Get the current arrow div
         const currentArrowDiv = document.querySelector('.current');
         if (currentArrowDiv) {
@@ -169,8 +160,6 @@ function handleKeyDownForGame(e) {
             };
             // Check if the pressed key matches the current arrow direction
             if (e.key === arrowKeyMap[currentArrow]) {
-                // Code to execute if the pressed key matches the arrow direction
-                const currentArrowDiv = document.querySelector('.current');
                 moveCurrentArrow(currentArrowDiv);
 
                 // Check if the current arrow div is the last arrow div
@@ -178,8 +167,11 @@ function handleKeyDownForGame(e) {
                     secondsLeft += 3;
                     if (stratagemID < roundStratagemList.length - 1) {
                         stratagemID += 1;
-                        createContainer();
+                        updateStratagemDisplay();
+                        const container = createContainer();
+                        document.body.appendChild(container);
                     } else {
+                        clearInterval(timerInterval);
                         postRoundSummaryScreen.style.display = 'block';
                         document.removeEventListener('keydown', handleKeyDownForGame);
                         inGameScreen.style.display = 'none';
@@ -192,22 +184,24 @@ function handleKeyDownForGame(e) {
                         postRoundTimeout = setTimeout(function () {
                             postRoundSummaryScreen.style.display = 'none';
                             preRoundGetReadyScreen.style.display = 'block';
-                            // Set second timeout inside the first timeout callback
                             setTimeout(function () {
                                 preRoundGetReadyScreen.style.display = 'none';
                                 inGameScreen.style.display = 'block';
                                 document.querySelector('.gameScreen').style.display = 'block';
                                 startRound();
-                                createContainer();
-                                createArrowDivs();
+                                updateStratagemDisplay();
+                                const container = createContainer();
+                                document.body.appendChild(container);
                                 document.addEventListener('keydown', handleKeyDownForGame);
+                                // secondsLeft = 5;
+                                // timeDisplay.textContent = secondsLeft;
+                                // timeDisplay.textContent = '00:0' + secondsLeft;
                             }, 2000);
                         }, 3000);
                     }
                     currentScore += document.querySelector('.gameScreen').getElementsByTagName('div').length * 100 * (secondsLeft / (59 * Math.PI));
                     playerScore.textContent = Math.round(currentScore);
                     gameRound.textContent = `Round ${currentRound}`;
-                    // timeDisplay.textContent = Number(timeDisplay.textContent.substring(timeDisplay.textContent.indexOf(':')+1,timeDisplay.textContent.length)) + 3;
                 }
             } else {
                 // If incorrect key is pressed, reset to the first arrow div
@@ -216,25 +210,17 @@ function handleKeyDownForGame(e) {
                 firstDiv.classList.add('current');
             }
         }
-    } else {
     }
 }
-
 
 // Function to move the current arrow div to the next arrow div
 function moveCurrentArrow(currentArrowDiv) {
     if (currentArrowDiv) {
-        // Remove .current class from the current div
         currentArrowDiv.classList.remove('current');
-
-        // Find the next sibling div
         const nextDiv = currentArrowDiv.nextElementSibling;
-
         if (nextDiv) {
-            // Add .current class to the next div
             nextDiv.classList.add('current');
         } else {
-            // If there's no next sibling, loop back to the first div
             const firstDiv = document.querySelector('.firstArrow');
             firstDiv.classList.add('current');
         }
@@ -242,7 +228,7 @@ function moveCurrentArrow(currentArrowDiv) {
 }
 
 function handleKeyDownForMenu(e) {
-    console.log('test')
+    console.log('test');
     const arrowKeyMap = {
         '↓': 'ArrowDown',
         '↑': 'ArrowUp',
@@ -259,12 +245,21 @@ function handleKeyDownForMenu(e) {
             initGame();
         }, 2000);
     } else {
-        console.log(`${e.key} is not in keymap`)
+        console.log(`${e.key} is not in keymap`);
     }
-};
+}
+
+function initMenu() {
+    gameOverLeaderboard.style.display = 'none';
+    document.addEventListener('keydown', handleKeyDownForMenu);
+    startScreen.style.display = 'block';
+    document.removeEventListener('keydown', handleKeyDownForGame);
+}
 
 initMenu();
 
 
+
 // Initialize the game
 // initGame();
+//functions that creates timer for a round
