839400ffa4d31120c69dea923dd0cf910b42725c
Tense11z <tensei.60@gmail.com>
2024-05-22 00:51:38 +0300

    started CSS

--- script.js
+++ script.js
@@ -1,88 +1,4 @@
 // arrows ← ↑ → ↓
-
-let stratagems;
-
-
-async function fetchStratagems() {
-    try {
-        const response = await fetch('stratagemList.json');
-        stratagems = await response.json();
-        initMenu(); // Initialize the menu after fetching the data
-    } catch (error) {
-        console.error('Error fetching stratagem data:', error);
-    }
-}
-// const stratagems = {
-//     // Supply: Backpacks
-//     'LIFT-850 Jump Pack': ['↓', '↑', '↑', '↓', '↑'],
-//     'B-1 Supply Pack': ['↓', '←', '↓', '↑', '↑', '↓'],
-//     'AX/LAS-5 "Guard Dog" Rover': ['↓', '↑', '←', '↑', '→', '→'],
-//     'SH-20 Ballistic Shield Backpack': ['↓', '←', '↓', '↓', '↑', '←'],
-//     'SH-32 Shield Generator Pack': ['↓', '↑', '←', '→', '←', '→'],
-//     'AX/AR-23 "Guard Dog"': ['↓', '↑', '←', '↑', '→', '↓'],
-//     // Supply: Support Weapons
-//     'MG-43 Machine Gun': ['↓', '←', '↓', '↑', '→'],
-//     'APW-1 Anti-Materiel Rifle': ['↓', '←', '→', '↑', '↓'],
-//     'M-105 Stalwart': ['↓', '←', '↓', '↑', '↑', '←'],
-//     'EAT-17 Expendable Anti-tank': ['↓', '↓', '←', '↑', '→'],
-//     'GR-8 Recoilless Rifle': ['↓', '←', '→', '→', '←'],
-//     'FLAM-40 Flamethrower': ['↓', '←', '↑', '↓', '↑'],
-//     'AC-8 Autocannon': ['↓', '←', '↓', '↑', '↑', '→'],
-//     'MG-206 Heavy Machine Gun': ['↓', '←', '↑', '↓', '↓'],
-//     'RS-422 Railgun': ['↓', '→', '↓', '↑', '←', '→'],
-//     'FAF-14 SPEAR Launcher': ['↓', '↓', '↑', '↓', '↓'],
-//     'GL-21 Grenade Launcher': ['↓', '←', '↑', '←', '↓'],
-//     'LAS-98 Laser Cannon': ['↓', '←', '↓', '↑', '←'],
-//     'ARC-3 Arc Thrower': ['↓', '→', '↓', '↑', '←', '←'],
-//     'LAS-99 Quasar Cannon': ['↓', '↓', '↑', '←', '→'],
-//     'RL-77 Airburst Rocket Launcher': ['↓', '↑', '↑', '←', '→'],
-//     // Supply: Other
-//     'EXO-45 Patriot Exosuit': ['←', '↓', '→', '↑', '←', '↓', '↓'],
-//     // Mission Stratagems
-//     'Reinforce': ['↑', '↓', '→', '←', '↑'],
-//     'SOS Beacon': ['↑', '↓', '→', '↑'],
-//     'Resupply': ['↓', '↓', '↑', '→'],
-//     'NUX-223 Hellbomb': ['↓', '↑', '←', '↓', '↑', '→', '↓', '↑'],
-//     'SSSD Delivery': ['↓', '↓', '↓', '↑', '↑'],
-//     'Seismic Probe': ['↑', '↑', '←', '→', '↓', '↓'],
-//     'Upload Data': ['←', '→', '↑', '↑', '↑'],
-//     'Eagle Rearm': ['↑', '↑', '←', '↑', '→'],
-//     'SEAF Artillery': ['→', '↑', '↑', '↓'],
-//     'Super Earth Flag': ['↓', '↑', '↓', '↑'],
-//     // Defensive
-//     'E/MG-101 HMG Emplacement': ['↓', '↑', '←', '→', '→', '←'],
-//     'FX-12 Shield Generator Relay': ['↓', '↓', '←', '→', '←', '→'],
-//     'A/ARC-3 Tesla Tower': ['↓', '↑', '→', '↑', '←', '→'],
-//     'MD-6 Anti-Personnel Minefield': ['↓', '←', '↑', '→'],
-//     'MD-I4 Incendiary Mines': ['↓', '←', '←', '↓'],
-//     'A/MG-43 Machine Gun Sentry': ['↓', '↑', '→', '→', '↑'],
-//     'A/G-16 Gatling Sentry': ['↓', '↑', '→', '←'],
-//     'A/M-12 Mortar Sentry': ['↓', '↑', '→', '→', '↓'],
-//     'A/AC-8 Autocannon Sentry': ['↓', '↑', '→', '↑', '←', '↑'],
-//     'A/MLS-4X Rocket Sentry': ['↓', '↑', '→', '→', '←'],
-//     'A/M-23 EMS Mortar Sentry': ['↓', '↑', '→', '↓', '→'],
-//     // Offensive: Orbital
-//     'Orbital Gatling Barrage': ['→', '↓', '←', '↑', '↑'],
-//     'Orbital Airburst Strike': ['→', '→', '→'],
-//     'Orbital 120MM HE Barrage': ['→', '→', '↓', '←', '→', '↓'],
-//     'Orbital 380MM HE Barrage': ['→', '↓', '↑', '↑', '←', '↓', '↓'],
-//     'Orbital Walking Barrage': ['→', '↓', '→', '↓', '→', '↓'],
-//     'Orbital Laser': ['→', '↓', '↑', '→', '↓'],
-//     'Orbital Railcannon Strike': ['→', '↑', '↓', '↓', '→'],
-//     'Orbital Precision StrikeOrbital Gas Strike': ['→', '→', '↑'],
-//     'Orbital Gas Strike': ['→', '→', '↓', '→'],
-//     'Orbital EMS Strike': ['→', '→', '←', '↓'],
-//     'Orbital Smoke Strike': ['→', '→', '↓', '↑'],
-//     //Offensive: Eagle
-//     'Eagle Strafing Run': ['↑', '→', '→'],
-//     'Eagle Airstrike': ['↑', '→', '↓', '→'],
-//     'Eagle Cluster Bomb': ['↑', '→', '↓', '↓', '→'],
-//     'Eagle Napalm Airstrike': ['↑', '→', '↓', '↑'],
-//     'Eagle Smoke Strike': ['↑', '→', '↑', '↓'],
-//     'Eagle 110MM Rocket Pods': ['↑', '→', '↑', '←'],
-//     'Eagle 500kg Bomb': ['↑', '→', '↓', '↓', '↓']
-// };
-
 // html elements selectors
 let gameRound = document.querySelector('.gameRound');
 let stratagemNameDisplay = document.querySelector('.stratagemNameDisplay');
@@ -119,6 +35,19 @@ let interval;
 let secondsLeft;
 let timerInterval;
 let previousSecondsLeft = initialTime;
+
+let stratagems;
+
+
+async function fetchStratagems() {
+    try {
+        const response = await fetch('stratagemList.json');
+        stratagems = await response.json();
+        initMenu(); // Initialize the menu after fetching the data
+    } catch (error) {
+        console.error('Error fetching stratagem data:', error);
+    }
+}
 
 timeBar.style.width = '100%';
 
